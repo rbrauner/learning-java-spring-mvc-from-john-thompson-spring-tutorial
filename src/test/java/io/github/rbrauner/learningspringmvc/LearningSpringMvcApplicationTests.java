f955dce75a43dfb660244813e5f69a6a8d209b74
rbrauner <rafalbrauner@gmail.com>
2019-11-17 21:04:17 +0100

    Format files

--- src/test/java/io/github/rbrauner/learningspringmvc/LearningSpringMvcApplicationTests.java
+++ src/test/java/io/github/rbrauner/learningspringmvc/LearningSpringMvcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LearningSpringMvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
