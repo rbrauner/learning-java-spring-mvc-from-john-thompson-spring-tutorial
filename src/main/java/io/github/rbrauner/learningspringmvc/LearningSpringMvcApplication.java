dd9cc16ac027df90efff12bb2cb3c5e04da64845
rbrauner <rafalbrauner@gmail.com>
2019-12-03 21:45:26 +0100

    Empty project

--- src/main/java/io/github/rbrauner/learningspringmvc/LearningSpringMvcApplication.java
+++ src/main/java/io/github/rbrauner/learningspringmvc/LearningSpringMvcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningSpringMvcApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LearningSpringMvcApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringMvcApplication.class, args);
+	}
 
 }
